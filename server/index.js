06424cea4f8abf58d48d5f38b9f31eb99e84364c
zhaolongfei-shareye <296180895+zhaolongfei-shareye@users.noreply.github.com>
2026-08-17 12:57:46 +0800

    fix: serve SoloHQ at the site root

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 export default {
   fetch(request, env) {
-    return env.ASSETS.fetch(request);
+    const url = new URL(request.url);
+    if (url.pathname === "/") {
+      url.pathname = "/index.html";
+    }
+    return env.ASSETS.fetch(new Request(url, request));
   },
 };
